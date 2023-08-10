0d600e7c6ea68df0a9c437a719fb93a5bac82fb7
Shreya-S-Vi <113327662+Shreya-S-Vi@users.noreply.github.com>
2023-08-10 13:14:46 +0530

    Update Exp 31 - Mean, Median, Mode.R

--- Exp 31 - Mean, Median, Mode.R
+++ Exp 31 - Mean, Median, Mode.R
@@ -1,4 +1,6 @@
 data <- c(90,50,70,80,70,60,20,30,80,90,20)
+
+#(i) Mean, Median and Mode
 data_mean <- mean(data)
 cat("Mean of the given vector:",data_mean,"\n")
 
@@ -10,4 +12,9 @@ mode <- function()
   return(names(sort(-table(data)))[1])
 }
 data_mode <- mode()
-cat("Mode of the given vector:",data_mode)
+cat("Mode of the given vector:",data_mode,"\n")
+
+#(ii) 2nd highest and 3rd Lowest value
+data <- unique(data)
+cat("2nd highest value of the given vector:",sort(data)[length(data)-1],"\n")
+cat("3rd lowest value of the given vector:", sort(data)[3])
